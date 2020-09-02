1c8bed39671d8811ceb4b7cbcaac176723f51218
Zackary Cammack <zack.cammack@gmail.com>
2020-09-02 20:17:02 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,4 +10,7 @@ class MP3Importer
     @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
   end
 
+  def import
+    self.files.each{|file| Song.new_by_filename(file)}
+  end
 end
