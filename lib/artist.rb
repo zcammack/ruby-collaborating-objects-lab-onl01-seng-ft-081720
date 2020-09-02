cf418b4cfa93591148956b767c7246333d323078
Zackary Cammack <zack.cammack@gmail.com>
2020-09-02 19:44:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,22 +26,12 @@ class Artist
   end
 
   def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
+    @@all.find { |artist| artist.name == name }
   end
 
   def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    self.find(name) ? self.find(name) : self.new(name)
   end
+
+
 end
