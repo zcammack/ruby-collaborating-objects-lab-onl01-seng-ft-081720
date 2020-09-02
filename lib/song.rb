d383d66a9e6affd5a7e45727fa404bf00cc8f2bb
Zackary Cammack <zack.cammack@gmail.com>
2020-09-02 20:18:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+      #find or create an artist instance
+      self.artist = Artist.find_or_create_by_name(name)
+      #assign it to the song attribute
+      artist.add_song(self)
+    end
+
+
+end
