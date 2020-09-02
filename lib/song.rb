8c5a0b5dca25571f7a201367b88972c3fa612ebe
Zackary Cammack <zack.cammack@gmail.com>
2020-09-02 20:02:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,8 +25,11 @@ class Song
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name
-    artist.add_song(self)
-  end
+      #find or create an artist instance
+      self.artist = Artist.find_or_create_by_name(name)
+      #assign it to the song attribute
+      artist.add_song(self)
+    end
+
 
 end
